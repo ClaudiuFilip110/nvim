14fa3e72d6f744a2c2989d61055736c67f8331b2
Claudiu Filip <cfil@skl.vc>
2026-01-09 14:23:12 +0000

    small cursor in insert mode

--- lua/clau/set.lua
+++ lua/clau/set.lua
@@ -5,7 +5,19 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 -- Cursor settings (make it small, not fat)
-vim.opt.guicursor = ""
+-- Thin cursor in insert mode, block in normal mode
+local function set_cursor(shape)
+  local esc = vim.api.nvim_replace_termcodes("<Esc>", true, false, true)
+  vim.api.nvim_echo({{esc .. "[" .. shape .. " q"}}, false, {})
+end
+
+vim.api.nvim_create_autocmd("InsertEnter", {
+  callback = function() set_cursor("6") end, -- Steady bar (thin)
+})
+
+vim.api.nvim_create_autocmd("InsertLeave", {
+  callback = function() set_cursor("1") end, -- Blinking block
+})
 
 -- Indentation (4 spaces)
 vim.opt.tabstop = 4
